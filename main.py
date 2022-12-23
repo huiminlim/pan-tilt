41c69873e8c6f2ec916f163adb1b2bd15858b695
huiminlim <e0036379@u.nus.edu>
2022-12-23 22:02:00 +0800

    Disable warnings

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import RPi.GPIO as GPIO
 import time
 
 servoPIN = 13
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+p.start(0) # Initialization
 try:
   while True:
     p.ChangeDutyCycle(5)
