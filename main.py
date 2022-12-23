df33f54ae07740b888efea9585500e3323f9b557
huiminlim <e0036379@u.nus.edu>
2022-12-23 19:21:31 +0800

    Add test code

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import RPi.GPIO as GPIO
+import time
 
-# duty cycle, calibrate if needed
-MIN_DUTY = 5
-MAX_DUTY = 10
+servoPIN = 13
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-servo_signal_pin = 13
-
-def deg_to_duty(deg):
-    return (deg - 0) * (MAX_DUTY- MIN_DUTY) / 180 + MIN_DUTY
-
-if __name__ == "__main__":
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-
-    GPIO.setup(servo_signal_pin, GPIO.OUT)
-    # set pwm signal to 50Hz
-    servo = GPIO.pwm(servo_signal_pin, 50)
-    servo.start(0)
-
-    # loop from 0 to 180
-    for deg in range(181):
-        duty_cycle = deg_to_duty(deg)
-        servo.ChangeDutyCycle(duty_cycle)
-
-    # cleanup the gpio pins
-    GPIO.cleanup()
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
